360d761705d04709568542bf890a55bc0c52486d
Admin <Admin@DESKTOP-RO9G5EO>
2024-04-06 12:07:48 +0200

    Jon conflicto commit

--- src/proapariciolima/ProAparicioLima.java
+++ src/proapariciolima/ProAparicioLima.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
     System.out.println("Este es el segundo cambio");
     System.out.println("Este es el tercer Cambio");
     System.out.println("Este es el cuarto cambio");
+    System.out.println("Cambio Jon conflicto");
 }  
 }
