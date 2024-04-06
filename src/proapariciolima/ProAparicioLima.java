428db512055686e8932032792292b1cc6bc55cd0
Ralfgamer50 <Ralfgamer50@DESKTOP-9D4A1CE.home>
2024-04-06 11:17:38 +0200

    Segundo commit

--- src/proapariciolima/ProAparicioLima.java
+++ src/proapariciolima/ProAparicioLima.java
@@ -4,5 +4,6 @@ public class ProAparicioLima {
 public static void main(String[] args) {
     
     System.out.println("Somos Jon y Ugaitz"); 
+    System.out.println("Este es el segundo cambio");
 }  
 }
